0990ab0222473cb4abc0ecaebcb8c42d2f40432b
breilyncj <Breilyn.ContrerasJ@gmail.com>
2025-06-08 22:59:11 -0600

    Creacion prueba Sidenav
    Creacion componente Sidenav

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import {SidenavComponent} from './components/sidenav-component/sidenav-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet ,SidenavComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
