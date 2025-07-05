121c9dbdab82070c71a7741b3e39ea30149ba454
breilyncj <Breilyn.ContrerasJ@gmail.com>
2025-07-05 02:45:35 -0600

    Actualizacion en inicio
    Ahora se ven cuantas tareas y notificaciones realmente tiene la persona

--- src/app/components/home-component/home-component.ts
+++ src/app/components/home-component/home-component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LoginService} from '../../services/login-service';
+import {TareasService} from '../../services/tareas-service';
+import {Tarea} from '../../models/tarea.model';
+import {UsuarioService} from '../../services/usuario-service';
+import {Notificacion} from '../../models/notificacion.model';
 
 @Component({
   selector: 'app-home-component',
@@ -11,11 +15,40 @@ import {LoginService} from '../../services/login-service';
 export class HomeComponent implements OnInit{
 
   nombreUsuario: string | null = null;
+  pendingTasksCount: number = 0;
+  createdTasksCount: number = 0;
+  activePermissionsCount: number = 0;
+  notificationsCount: number = 0;
 
-  constructor(private loginService: LoginService) {}
+  constructor(
+    private loginService: LoginService,
+    private tareasService: TareasService,
+    private usuarioService: UsuarioService,
+  ) {}
+
+  loadTaskStatistics(userId: number): void {
+    this.tareasService.getAllByIdUsuarioCreador(userId).subscribe((tareas: Tarea[]) => {
+      // Filtrar las tareas creadas
+      this.createdTasksCount = tareas.length;
+
+      // Filtrar las tareas pendientes
+      this.pendingTasksCount = tareas.filter(t => t.estado === 'En espera').length;
+
+
+      this.activePermissionsCount = tareas.filter(t => t.estado === 'Finalizada').length;
+    });
+
+    this.usuarioService.getNotificacionesRecibidasPor(userId).subscribe((notificaciones: Notificacion[]) => {
+      this.notificationsCount = notificaciones.length;
+    });
+  }
 
   ngOnInit(): void {
     const usuario = this.loginService.getUser();
     this.nombreUsuario = usuario?.cT_Nombre_usuario || 'Usuario';
+
+    if (usuario) {
+      this.loadTaskStatistics(usuario.cN_Id_usuario);
+    }
   }
 }
